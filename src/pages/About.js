07c2d9848388e62f218f5fe265a011c52b029a45
yigittin <i.yigittin@hotmail.com>
2021-07-12 03:30:40 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam nostrum, dicta necessitatibus facilis illo optio unde ex officiis hic labore. Animi totam perferendis facere, odit a quas cupiditate ut tempora nisi vero sequi enim delectus sunt quia repellendus minima incidunt.
+      </p>
+    </section>
+  )
+}
+
+export default About
